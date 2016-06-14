7a6b40464e2e3af09d3da671cac8adafd8cdf740
Alan Espinosa <alanesp92@hotmail.com>
2016-06-14 16:15:26 -0500

    Módulo de login y conexión a través de PHP con la base de datos

--- php/login.php
+++ php/login.php
@@ -1,15 +1,45 @@
 <?php
+include_once 'config.php';
 $data = $_POST['data'];
 
-$loginData = json_decode($data);
+//$loginData = json_decode($data);
 //$loginData->username;
 
+// Entrada de datos a mano
+$loginData = array(
+    "username" => "alanesp92@hotmail.com",
+    "password" => "centroakumal"
+    );
+
+
+/* Plantilla para Json
 $response = [
     "success" => true,
     "userType" => 1,
     "name" => "estupido",
     "reason" => "no te importa"
 ];
+*/
+
+$sql = "SELECT nombre, mail, pass, Perfil_id FROM Usuario WHERE mail='" . $loginData["username"] . "' AND pass='" .$loginData["password"] . "'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) { 
+    $response = [
+    "success" => true,
+    "userType" => $row["Perfil_id"],
+    "name" => $row["nombre"]
+    ];
+       
+    }
+} else {
+    $response = [
+    "success" => false,
+    "reason" => "Usuario y/o contraseña inválido"
+];
+}
 
 echo json_encode($response); 
 ?>
