28b62c05072ae3d433c67ee5f4e2ea8fed51d323
Iker Llorens <iker.llorens.13@gmail.com>
2016-06-17 15:09:13 -0500

    Implementando la función de retomar la sesión si sea token es válido

--- app/main-app.service/main-app.service.ts
+++ app/main-app.service/main-app.service.ts
@@ -16,36 +16,43 @@
  */
 
 import {Injectable} from '@angular/core'
-import { Http} from '@angular/http'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
 
 import { Subject }    from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable'
 import '../rxjs-operators'
 
 import {LoginResponse} from '../classes/LoginObject.class/LoginObject.class'
+import { Main } from '../main-app/main-app'
 
 @Injectable()
 export class MainScreenService {
     private loggedInObservable: Subject<boolean> = new Subject<boolean>()
     private loginInfoObservable: Subject<LoginResponse> = new Subject<LoginResponse>()
-    loggedInObservable$ = this.loggedInObservable.asObservable()
-    loginInfoObservable$ = this.loginInfoObservable.asObservable();
+    public loggedInObservable$ = this.loggedInObservable.asObservable()
+    public loginInfoObservable$ = this.loginInfoObservable.asObservable();
+    public checkLoginObservable$: Observable<LoginResponse> = new Observable<LoginResponse>()
+    public valid_tokenObservable$: Observable<LoginResponse> = new Observable<LoginResponse>()
+
     private loggedIn: boolean
     private loginInfo: LoginResponse
+    private token: string
 
     constructor(private http: Http) {
-        
         this.loggedInObservable$.subscribe(
             loggedIn => this.loggedIn = loggedIn
         )
         this.loginInfoObservable$.subscribe(
             loginInfo => this.loginInfo = loginInfo
-        )
-        
-    }
-    
-    
-    
+                )
+//        this.checkLoginObservable$.sub        scribe(
+//            loginInfo => this.fetchUserInfo(loginInfo.s        uccess)
+//                )
+//        this.valid_tokenObservable$.sub        scribe(
+//            loginInfo => this.loginInfoObservable.next(log        inInfo)
+//        )
 
+    }
 
     setLoginInfo(loginInfo: LoginResponse): void {
         localStorage.setItem("token_CEA", loginInfo.token)
@@ -55,11 +62,44 @@ export class MainScreenService {
 
     checkLogin(): void {
         var token = localStorage.getItem("token_CEA")
+
         console.info("token: " + token)
         if (token != null) {
+            let headers = new Headers({ 'Content-Type': 'application/json' });
+            let options = new RequestOptions({ headers: headers });
+
+            this.token = token
+            this.http.post(Main.serverUrl + 'checklog.php', JSON.stringify(token), options)
+                .map(this.extractData)
+                .subscribe(
+                    loginInfo => this.fetchUserInfo(loginInfo.success)
+                )
+
             // TODO: PHP login_check con Service
-            console.info(".|.") 
-            this.loggedInObservable.next(true);
+            console.info(".|.")
+            //this.loggedInObservable.next(true);
         }
     }
+
+    private fetchUserInfo(valid_token: boolean): void {
+        if (valid_token) {
+            let headers = new Headers({ 'Content-Type': 'application/json' });
+            let options = new RequestOptions({ headers: headers });
+            this.http.post(Main.serverUrl + 'tokenInfo.php', JSON.stringify(this.token), options)
+                .map(this.extractData)
+                .subscribe(
+                    loginInfo => this.loginInfoObservable.next(loginInfo)
+                )
+            this.loggedInObservable.next(true)
+        } else {
+            this.loggedInObservable.next(false)
+        }
+
+    }
+
+    private extractData(res: Response) {
+        let responseJSON = res.json();
+        return responseJSON;
+    }
+
 }
