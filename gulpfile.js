e6584d3bf58eae706901780343794917e4a85d88
Iker Llorens <iker.llorens.13@gmail.com>
2016-06-19 12:32:45 -0500

    Experimentos en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,73 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var gulp = require('gulp')
-    rename = require('gulp-rename'),
-    traceur = require('gulp-traceur'),
-    webserver = require('gulp-webserver');
-    watch = require('gulp-watch');
 
-// run init tasks
-gulp.task('default', ['dependencies', 'js', 'html', 'css']);
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-// run development task
-gulp.task('dev', ['watch', 'serve']);
-
-// serve the build dir
-gulp.task('serve', function () {
-  gulp.src('build')
-    .pipe(webserver({
-      open: true
-    }));
+gulp.task('lint', function () {
+    return gulp.src('app/**/*.js');
 });
 
-// watch for changes and run the relevant task
-gulp.task('watch', function () {
-  gulp.watch('src/**/*.js', ['js']);
-  gulp.watch('src/**/*.html', ['html']);
-  gulp.watch('src/**/*.css', ['css']);
-});
-
-// move dependencies into build dir
-gulp.task('dependencies', function () {
-  return gulp.src([
-    'node_modules/angular2/**'
-  ])
-    .pipe(gulp.dest('build/node_modules'));
-});
-
-// transpile & move js
-gulp.task('js', function () {
-  return gulp.src('app/**/*.js')
-    .pipe(rename({
-      extname: ''
-    }))
-    .pipe(traceur({
-      modules: 'instantiate',
-      moduleName: true,
-      annotations: true,
-      types: true,
-      memberVariables: true
-    }))
-    .pipe(rename({
-      extname: '.js'
-    }))
-    .pipe(gulp.dest('build'));
-});
-
-// move html
-gulp.task('html', function () {
-  return gulp.src('app/**/*.html')
-    .pipe(gulp.dest('build'))
-});
-
-// move css
-gulp.task('css', function () {
-  return gulp.src('app/**/*.css')
-    .pipe(gulp.dest('build'))
-});
-
-gulp.task('html', function () {
-   return gulp.src('index.html')
-           .pipe(gulp.dest('build')) 
+gulp.task('scripts', function() {
+    return gulp.src('app/**/*.js')
+        //.pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/app'));
 });
